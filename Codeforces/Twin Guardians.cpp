0b3dd3db43bb4709e8044cb7cd53f9bfe09ce39c
Saief Md. Adnan <saiefadnan078@gmail.com>
2025-09-29 14:07:14 +0600

    Create Twin Guardians.cpp

---
+++ Codeforces/Twin Guardians.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define F first
+#define S second
+
+// Debugging (only for local, disable before submission)
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+#define dbg(x)
+#endif
+
+// Common constants
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const ld EPS = 1e-9;
+
+// Type aliases
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+
+// Functions
+ll modpow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    a %= m;
+    while(b > 0) {
+        if(b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+bool isPrime(ll a){
+    if(a==2 || a==3) return true;
+    if(a<=1 || a % 2==0 || a%3==0) return false;
+    for(ll i=5;i*i<=a;i+=6){ 
+        if(a%i==0 || a%(i+2)==0) return false;
+    }
+    return true;
+}
+
+void solve() {
+    // Write per-testcase logic here
+    ll a, b;
+    cin>>a>>b;
+    if(a+2!=b) {
+        cout<<"N\n";
+    }
+    else if(isPrime(a) && isPrime(b)){
+        cout<<"Y\n";
+    }
+    else{
+        cout<<"N\n";
+    }
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    cin >> t; // Uncomment if multiple testcases
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
